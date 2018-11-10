77b6892d8bedc5b3a8dcace9f1ab736f1ee97470
Milana Lima dos Santos <milanals@usp.br>
2018-11-09 22:15:48 -0200

    Add files via upload

---
+++ is_octave.m
@@ -0,0 +1,9 @@
+%% From wiki.octave.org/Compatibility
+%% subfunction that checks if we are in octave
+ function r = is_octave()
+   persistent x;
+   if (isempty (x))
+     x = exist ('OCTAVE_VERSION', 'builtin');
+   end
+   r = x;
+ end
